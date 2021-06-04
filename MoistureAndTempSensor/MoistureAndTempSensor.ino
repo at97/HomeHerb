1ee476256901ef533b4a359e22da8eec9c17225d
at97 <35437308+at97@users.noreply.github.com>
2021-06-03 23:06:39 -0400

    Test moisture and humidity/temperature sensors

---
+++ MoistureAndTempSensor/MoistureAndTempSensor.ino
@@ -0,0 +1,54 @@
+#include "DHTesp.h"
+
+DHTesp dht;
+
+int val = 0; //value for storing moisture value 
+int soilPin = A1;//Declare a variable for the soil moisture sensor 
+int soilPower = 23;//Variable for Soil moisture Power
+
+//Rather than powering the sensor through the 3.3V or 5V pins, 
+//we'll use a digital pin to power the sensor. This will 
+//prevent corrosion of the sensor as it sits in the soil. 
+
+void setup() 
+{
+    Serial.begin(9600);   // open serial over USB
+    
+    pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
+    digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
+
+    dht.setup(7, DHTesp::DHT22);
+}
+
+void loop() 
+{
+    Serial.print("Soil Moisture = ");    
+    //get soil moisture value from the function below and print it
+    Serial.println(readSoil());
+
+    //This 1 second timefrme is used so you can test the sensor and see it change in real-time.
+    //For in-plant applications, you will want to take readings much less frequently.
+    delay(1000);//take a reading every second
+
+    //RH & Temp sensor
+    TempAndHumidity measurement = dht.getTempAndHumidity();
+
+    Serial.print("Temperature: ");
+    Serial.println(measurement.temperature);
+
+    Serial.print("Humidity: ");
+    Serial.println(measurement.humidity);
+
+    delay(5000);
+}
+
+//This is a function used to get the soil moisture content
+int readSoil()
+{
+
+    digitalWrite(soilPower, HIGH);//turn D7 "On"
+    delay(10);//wait 10 milliseconds 
+    val = analogRead(soilPin);//Read the SIG value form sensor 
+    digitalWrite(soilPower, LOW);//turn D7 "Off"
+    return val;//send current moisture value
+}
